cb5514cbc3d04545a0340424181dc80f73ef6e05
JunnyJun <chbe5082@naver.com>
2023-07-10 22:05:57 +0900

    feat : init

---
+++ src/main/kotlin/git/io/release/ReleaseApplication.kt
@@ -0,0 +1,11 @@
+package git.io.release
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReleaseApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReleaseApplication>(*args)
+}
---
+++ src/test/kotlin/git/io/release/ReleaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.io.release
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReleaseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
