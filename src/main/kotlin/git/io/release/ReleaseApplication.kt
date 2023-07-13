56d9b7dfd828e07b3f17d1948f34db13c4d107a5
Junny <72375574+Junnyjun@users.noreply.github.com>
2023-07-13 23:15:26 +0900

    Update ReleaseApplication.kt

--- src/main/kotlin/git/io/release/ReleaseApplication.kt
+++ src/main/kotlin/git/io/release/ReleaseApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigClient
 class ReleaseApplication
 
 fun main(args: Array<String>) {
